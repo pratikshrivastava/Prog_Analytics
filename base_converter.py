9cef32bba8cc577b275e4a34c6f52a62364100a0
pratikshrivastava <pratikshrivastava23@gmail.com>
2018-01-02 19:52:44 -0600

    Prog Analytics, assignment 1

--- base_converter.py
+++ base_converter.py
@@ -14,17 +14,15 @@ while (True):
     elif convert[0] in ['x', 'X']:
         print ('Thanks for playing! Existing the program now!')
         break
-    else:
-        print ('Incorrect options:')
 
     if convert[0] in ['b', 'B']:
-        num_in = int (input ("Type the integer you want to convert to the base 2."))
+        num_in = int (input ("Type the integer you want to convert to the base 2."), 10)
         print ('The integer ', num_in, ' in binary : {0:b}'.format (num_in))
     elif convert[0] in ['h', 'H']:
-        num_in = int (input ("Type the integer you want to convert to the base 16."))
+        num_in = int (input ("Type the integer you want to convert to the base 16."), 16)
         print ('The integer ', num_in, ' in hexadecimal : {0:x}'.format (num_in))
     elif convert[0] in ['d', 'D']:
-        num_in = int (input ("Type the integer you want to convert to the base 10."))
+        num_in = int (input ("Type the integer you want to convert to the base 10."), 10)
         print ('The integer ', num_in, ' in decimal : {0:d}'.format (num_in))
     else:
         print ("Something is wrong with our code")
